ddc8924d7d70c6fdbbf865debef54141ede797f9
Fabian <fabian.solano@ridgerun.com>
2021-02-09 15:52:09 -0600

    media_server.sh: Add copyright header

--- media-server/media_server.sh
+++ media-server/media_server.sh
@@ -1,3 +1,12 @@
+# Copyright (C) <year> RidgeRun, LLC (http://www.ridgerun.com)
+#
+# Author: Fabian Solano <fabian.solano@ridgerun.com>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+
+
 # Load settings
 source config.txt
 
